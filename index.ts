10553106eb173445c58d5394f744ec12e1bca2fd
Yony Calsin <helloyonycalsin@gmail.com>
2021-02-24 09:57:32 -0500

    Revert "chore: added types from Next.js (#2)"
    This reverts commit f247be02ea04931cb8b6bcd291e6bad7a9148182.

--- index.ts
+++ index.ts
@@ -1,17 +1,15 @@
 import cors, { CorsOptions, CorsOptionsDelegate } from 'cors';
-import { NextApiRequest, NextApiResponse } from 'next';
-import express = require('express');
+
+interface More {
+   [key: string]: any;
+}
 
 // Helper method to wait for a middleware to execute before continuing
 // And to throw an error when an error happens in a middleware
 function initMiddleware(middleware) {
-   return (
-      req: NextApiRequest,
-      res: NextApiResponse,
-      options?: CorsOptions | CorsOptionsDelegate,
-   ) =>
+   return (req: More, res: More, options?: CorsOptions | CorsOptionsDelegate) =>
       new Promise((resolve, reject) => {
-         middleware(options)(req, res, (result: express.RequestHandler) => {
+         middleware(options)(req, res, (result) => {
             if (result instanceof Error) {
                return reject(result);
             }
