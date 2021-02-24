14984b2649f4d87ce32b558cc15feed17b98173c
Yony Calsin <helloyonycalsin@gmail.com>
2021-02-24 09:47:39 -0500

    feat: 🎸 update

--- index.d.ts
+++
@@ -1,6 +0,0 @@
-import { CorsOptions, CorsOptionsDelegate } from 'cors';
-interface More {
-    [key: string]: any;
-}
-declare const NextCors: (req: More, res: More, options?: CorsOptions | CorsOptionsDelegate) => Promise<unknown>;
-export default NextCors;
