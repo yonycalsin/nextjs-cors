adaf5b2e047918c271bfd8bb91a45ce668941a0f
Lorenzo Signoretti <lorenzosigno@gmail.com>
2021-03-22 18:41:10 +0100

    refactor: add typescript typing. (#24)

--- src/index.ts
+++ src/index.ts
@@ -6,14 +6,14 @@ import type {
 
 // Helper method to wait for a middleware to execute before continuing
 // And to throw an error when an error happens in a middleware
-function initMiddleware(middleware) {
+function initMiddleware(middleware: typeof cors) {
    return (
       req: NextApiRequest,
       res: NextApiResponse,
       options?: CorsOptions | CorsOptionsDelegate,
-   ) => {
+   ) =>
       new Promise((resolve, reject) => {
-         middleware(options)(req, res, (result) => {
+         middleware(options)(req, res, (result: Error | unknown) => {
             if (result instanceof Error) {
                return reject(result);
             }
@@ -21,7 +21,6 @@ function initMiddleware(middleware) {
             return resolve(result);
          });
       });
-   };
 }
 
 // You can read more about the available options here: https://github.com/expressjs/cors#configuration-options
